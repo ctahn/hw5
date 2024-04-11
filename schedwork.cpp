f65173a5cfe57a752be2bbf0d0e6b681629f40f1
Christopher Ahn <ctahn@usc.edu>
2024-04-11 07:12:12 +0000

    HW5

--- schedwork.cpp
+++ schedwork.cpp
@@ -20,10 +20,61 @@ using namespace std;
 static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
-// Add prototypes for any helper functions here
+bool scheduleHelper(const AvailabilityMatrix& avail, const size_t dailyNeed, const size_t maxShifts, vector<int>& shifts, size_t r, size_t c);
+bool isValid(const size_t maxShifts, vector<int>& shifts);
+
+bool scheduleHelper(
+    const AvailabilityMatrix& avail, 
+    const size_t dailyNeed, 
+    const size_t maxShifts, 
+    DailySchedule& sched, 
+    vector<int>& shifts,
+    size_t r, size_t c)
+{
+  
+  if(r == avail.size()){
+    return isValid(maxShifts, shifts);
+  }
+    for(size_t j=0; j < avail[0].size(); j++){
+      if(avail[r][j] == 1 && find(sched[r].begin(), sched[r].end(), j) == sched[r].end()){
+        sched[r][c] = Worker_T(j);
+        shifts[j] += 1;
+        if(isValid(maxShifts, shifts)){
+          if(r < avail.size() && c < dailyNeed-1){
+
+            if(scheduleHelper(avail, dailyNeed, maxShifts, sched, shifts, r, ++c)){
+              return true;
+            }
+            c--;
+          }
+          else if(r < avail.size() && c == dailyNeed-1){
+            if(scheduleHelper(avail, dailyNeed, maxShifts, sched, shifts, ++r, 0)){
+              return true;
+            }
+            r--;
+          } 
+        }
+      
+      sched[r][c] = INVALID_ID;
+      shifts[j] -= 1;
+      }
+    }
+  return false;
+}
+
+bool isValid(const size_t maxShifts, 
+    vector<int>& shifts)
+{
+  
+  if(find(shifts.begin(), shifts.end(), int(maxShifts+1)) != shifts.end()){
+    return false;
+  }
+
+  return true;
+}
 
 
-// Add your implementation of schedule() and other helper functions here
+
 
 bool schedule(
     const AvailabilityMatrix& avail,
@@ -35,11 +86,17 @@ bool schedule(
     if(avail.size() == 0U){
         return false;
     }
+    int numWorkers = avail.size();
     sched.clear();
-    // Add your code below
-
-
-
-
+    sched.resize(avail.size());
+    for(size_t i=0; i < avail.size(); i++){
+      sched[i].resize(dailyNeed);
+      for(size_t j=0; j < sched[i].size(); j++){
+        sched[i][j] = INVALID_ID;
+      }
+    }
+    vector<int> shifts(numWorkers,0);
+    return scheduleHelper(avail, dailyNeed, maxShifts, sched, shifts, 0, 0);
+    
 }
 
--- wordle.cpp
+++ wordle.cpp
@@ -2,6 +2,7 @@
 // For debugging
 #include <iostream>
 // For std::remove
+#include <fstream>
 #include <algorithm> 
 #include <map>
 #include <set>
@@ -9,11 +10,14 @@
 
 #include "wordle.h"
 #include "dict-eng.h"
+#include <string>
 using namespace std;
 
 
 // Add prototypes of helper functions here
+void floatHelp(std::set<std::string>& list, std::string word, std::string floats);
 
+void helper(string word, std::set<std::string>& answers ,string alphabet, const set<string>& dictionary);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -21,8 +25,74 @@ std::set<std::string> wordle(
     const std::string& floating,
     const std::set<std::string>& dict)
 {
-    // Add your code here
-
+  set<string> dictionary;
+  ifstream file;
+  file.open("dict-eng.txt");
+  string line;
+  while(std::getline(file, line)){
+    dictionary.insert(line);
+  }
+  file.close();
+  
+  set<string> list;
+  if(floating.length() == 0){
+    list.insert(in);
+  }
+  floatHelp(list, in, floating);
+  set<string> answers;
+  for(string word : list)
+  {
+    helper(word, answers, "abcdefghijklmnopqrstuvwxyz", dictionary);
+  }
+  return answers;
 }
 
-// Define any helper functions here
+void floatHelp(std::set<std::string>& list, std::string word, std::string floats)
+{
+  if(floats.length() == 0){
+    return;
+  }
+  
+
+  if(floats.length() == 1){
+    for(size_t i=0; i < word.length(); i++){
+      if(word[i] == '-'){
+        word[i] = floats[0];
+        list.insert(word);
+        word[i] = '-';
+      }
+    }
+    return;
+  }
+  
+  for(size_t i=0; i < word.length(); i++){
+    if(word[i] == '-'){
+      word[i] = floats[0];
+      string temp = floats;
+      temp.erase(0,1);
+      floatHelp(list, word, temp);
+      word[i] = '-';
+    }
+  }
+}
+
+void helper(string word, std::set<std::string>& answers,string alphabet, const set<string>& dictionary)
+{
+  if(word.find('-') == string::npos){
+    if(dictionary.find(word) != dictionary.end()){
+      answers.insert(word);
+    }
+    return;
+  }
+  
+  for(size_t i=0; i < word.length(); i++){
+    if(word[i] == '-'){
+      for(size_t j=0; j < alphabet.length(); j++){
+        word[i] = alphabet[j];
+        helper(word, answers, alphabet, dictionary);
+        word[i] = '-';
+      }
+    }
+  }
+}
+
